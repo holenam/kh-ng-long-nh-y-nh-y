4bd5615ff4330beff9ef778a699983a51f1ea93f
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2020-06-01 23:07:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,73 @@
-function setup() { 
-    createCanvas(1200, 500);
-  } 
-let velocityX = 1;
-let velocityY = 0;
+var velocityX = 3;
+var velocityY = 0 ;
+var yJump = 0;
 
-let aX = 0.01;
-let aY = 0;
+var aX = 0.2;
+var aY = 0;
 
-let aCounter = 0;
+var g = 10;
+var aCounter = 0;
 
 let Rect = {
-    positionX: 250,
-    positionY: 450,
+    positionX: 150,
+    positionY: 350,
 }
-let Tgl = {
-    positionXa: 900,
-    positionYa: 470,
-    positionXb: 910,
-    positionYb: 450,
-    positionXc: 920,
-    positionYc: 470,
+let Obs = {
+    positionX: 750,
+    positionY: 350,
 }
 
+
+
+function setup() { 
+    createCanvas(850, 450);
+    
+} 
+
 function draw() {
-    background("#000000");
-    rect(Rect.positionX,Rect.positionY,20,20);
-    fill(250,250,250);
-    triangle(Tgl.positionXa,Tgl.positionYa,Tgl.positionXb,Tgl.positionYb,Tgl.positionXc,Tgl.positionYc);
-Tgl.positionXa += - velocityX
-Tgl.positionXb += - velocityX
-Tgl.positionXc += - velocityX
-Tgl.positionYa += - velocityY
-Tgl.positionYb += - velocityY
-Tgl.positionYc += - velocityY
+
+background(100,100,100);
+stroke(200);
+line(0,370,850,370);
+rect(Rect.positionX,Rect.positionY,20,20);
+fill(250,250,250);
+rect(Obs.positionX,Obs.positionY,20,20);
+
+Jump();
 
 aCounter++;
 if (aCounter >= 30) {
     velocityX += aX;
-    velocityY += aY;
     aCounter = 0;
+}
 
+Obs.positionX -= velocityX;
+function Jump() {
+    if (Rect.positionY + 20 < 360) {
+        yJump += 1;
+        onGround = false;
+    } else {
+        yJump = 0;
+        onGround = true;
+    }
 
+    Rect.positionY += yJump;
 
+    if (keyIsDown(ENTER)) {
+        if (onGround) {
+            yJump -= 10;
+            onGround = false;
+        }
+    }
+    Rect.positionY += yJump;
+}
+
+if (dist(Rect.positionX, Rect.positionY, Obs.positionX, Obs.positionY) < 20){
+    
 }
 }
+
+    
+
+
+
