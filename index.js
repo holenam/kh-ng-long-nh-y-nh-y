f230f6d237f0848590230330d9900121e7a04bc2
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2020-06-04 20:10:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var obstacles = [];
 
-var velocityX = 3;
+var velocityX = 4;
 var velocityY = 0 ;
 var yJump = 0;
 
@@ -9,6 +10,7 @@ var aY = 0;
 var aCounter = 0;
 
 var Score = 0;
+var SocreMultiplayer = 1;
 
 let Rect = {
     positionX: 150,
@@ -38,11 +40,6 @@ rect(Obs.positionX,Obs.positionY,20,20);
 Jump();
 stop();
 
-aCounter++;
-if (aCounter >= 30) {
-    velocityX += aX;
-    aCounter = 0;
-}
 
 Obs.positionX -= velocityX;
 function Jump() {
@@ -65,6 +62,10 @@ function Jump() {
     Rect.positionY += yJump;
 }
 
+Score += SocreMultiplayer;
+textSize(15);
+text("SCORE :  " +  Score, 850/2, 20);
+
 function stop() {
 if (dist(Rect.positionX, Rect.positionY, Obs.positionX, Obs.positionY) < 20){
     velocityX = velocityY = yJump = aX = aY =0;
@@ -73,15 +74,24 @@ if (dist(Rect.positionX, Rect.positionY, Obs.positionX, Obs.positionY) < 20){
     text("GAME OVER", 850/2, 450/2);
     textSize(15);
     text("Press F5 to restart", 850/2, 450/2.4);
+    SocreMultiplayer = 0;
+    console.log("Score");
 }}
 
-Score ++;
-textSize(15);
-text("SCORE :  " +  Score, 850/2, 20);
-
-}
-
+function UpdateObstacles() {
+    if (aCounter >= 30) {
+        velocityX += aX;
+        aCounter = 0;
+    }
     
-
+    if (dist(Rect.positionX, Rect.positionY, Obs.positionX, Obs.positionY) > 30 ) {
+        fill(250,250,250);
+        rect(Obs.positionX,Obs.positionY,20,20);
+    } else {
+        stop();
+    }
+    UpdateObstacles();
+}
+}
 
 
